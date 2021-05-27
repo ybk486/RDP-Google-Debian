60eb166d6bb62d76cd63b39be8421ccd6de35b45
ybk486 <ybk486@163.com>
2021-05-27 17:23:23 +0800

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m FSID
-sudo adduser FSID sudo
-echo 'FSID:8426' | sudo chpasswd
+sudo useradd -m kxmh
+sudo adduser kxmh sudo
+echo 'FSID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
